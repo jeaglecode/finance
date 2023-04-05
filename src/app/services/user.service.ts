9abef3e527b5e9771b77776b3c1ee264bd417e0d
jeaglecode <32748770+jeaglecode@users.noreply.github.com>
2023-04-05 16:44:15 -0700

    change to prod server url and added redirects

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private url = 'http://localhost:3000/users';
+  private url = 'https://financeserver.onrender.com/users';
 
   constructor(private http: HttpClient) { }
 
