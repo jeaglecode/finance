7e1bc673298efbfa476ecfb6fdd1d544aefd052f
jeaglecode <32748770+jeaglecode@users.noreply.github.com>
2023-04-06 15:20:46 -0700

    created retirement html page and added general styling to the site

--- src/app/retirementincome/retirementincome.component.ts
+++ src/app/retirementincome/retirementincome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from "../services/user.service";
 
 @Component({
   selector: 'app-retirementincome',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./retirementincome.component.css']
 })
 export class RetirementincomeComponent {
+  public profileData: any = [];
+  number = 0;
+
+constructor(private userService: UserService) {
+}
+
+
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((users: any) => {
+      this.profileData = users;
+      this.number = this.profileData.currentAge;
+
+      console.log(this.profileData);
+    });
+
+  }
+
+
+  changeNumber() {
+    this.number = this.profileData.currentAge;
+
+  }
 
 }
