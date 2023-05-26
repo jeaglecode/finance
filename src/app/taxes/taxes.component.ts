2544abc4817c3cc9fa7807176bc146a41477a913
jeaglecode <32748770+jeaglecode@users.noreply.github.com>
2023-05-25 22:05:35 -0700

    added taxes input and fixed banner scaling

--- src/app/taxes/taxes.component.ts
+++ src/app/taxes/taxes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from "../services/user.service";
 
 @Component({
   selector: 'app-taxes',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./taxes.component.css']
 })
 export class TaxesComponent {
+  public profileData: any = [];
+
+  currentAge = 0;
+  retirementAge = 0;// Can be typical or IUL
+  typicalAnnualContribution = 0;
+  typicalCurrentAccountBalance = 0;
+  typicalTaxRateBeforeRetirement = 0;
+
+
+
+
+
+  IULAnnualContribution = 0;
+  IULLumpSumPremium = 0;
+  IULTaxRateBeforeRetirement = 0;
+
+  constructor(private userService: UserService) {
+  }
+  ngOnInit() {
+    this.userService.getUsers().subscribe((users: any) => {
+      this.profileData = users;
+      this.retirementAge = this.profileData.retirementAge;
+      this.typicalAnnualContribution = this.profileData.contribution;
+      this.typicalCurrentAccountBalance = this.profileData.currentAccountBalance;
+      this.typicalTaxRateBeforeRetirement = this.profileData.incomeTaxRateDuringWorkingYears;
+
+      console.log(this.profileData);
+
+
+
+
+
+
+
+      this.IULAnnualContribution = this.profileData.annualPremium
+      this.IULLumpSumPremium = this.profileData.lumpSum;
+      this.IULTaxRateBeforeRetirement = this.profileData.incomeTaxRateBeforeRetirement;
+
+
+    });
+  }
+
+
 
 }
